4bd3fcbb82d5cc7d8854b5a4234a81cae1d9f5db
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-08-30 16:55:42 +0300

    Create table products with data & checks

--- fd_test.session.sql
+++ fd_test.session.sql
@@ -1,40 +1,57 @@
 --delete table
-DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "products";
 --create table
-CREATE TABLE "users"(
-  "firstName" VARCHAR(32) NOT NULL CHECK("firstName" != ''),
-  "lastName" VARCHAR(32) NOT NULL CHECK("lastName" != ''),
-  "email" VARCHAR(56) NOT NULL CHECK("email" != '') UNIQUE,
-  "isMale" BOOLEAN NOT NULL,
-  "birthday" DATE NOT NULL CHECK("birthday" <= current_date),
-  "height" NUMERIC(3, 2) NOT NULL CHECK(
-    "height" > 0
-    AND "height" < 2.30
-  )
+CREATE TABLE "products"(
+  "name" VARCHAR(32) NOT NULL UNIQUE CHECK("name" != ''),
+  "price" NUMERIC(5, 2) NOT NULL CHECK("price" >= 0),
+  "currency" VARCHAR(32) NOT NULL CHECK("currency" != ''),
+  "productionDate" DATE NOT NULL CHECK("productionDate" <= current_date),
+  "amount" INTEGER CHECK(
+    "amount" >= 0
+    AND "amount" <= 1000
+  ),
+  "helfLife" VARCHAR(32)
 );
 --insert data
-INSERT INTO "users"
+INSERT INTO "products"
 VALUES (
-    'Eduard',
-    'Tiutiunnyk',
-    'ediktyutyunnik@gmail.com',
-    true,
-    '1996-05-27',
-    1.76
+    'Milk',
+    30.50,
+    'ua',
+    '2022-08-28',
+    5,
+    'one week'
   ),
   (
-    'Ivan',
-    'Sobol',
-    'ivansobol@gmail.com',
-    true,
-    '1996-03-25',
-    1.70
+    'Chocolade',
+    25.75,
+    'ua',
+    '2022-06-20',
+    1,
+    'three month'
   ),
   (
-    'Tim',
-    'Pashkov',
-    'timpashkov@gmail.com',
-    true,
-    '1997-08-23',
-    1.74
+    'Solt',
+    15.65,
+    'ua',
+    '2021-08-28',
+    1,
+    'three years'
+  ),
+  (
+    'Bread',
+    22.55,
+    'ua',
+    '2022-08-25',
+    2,
+    'five days'
+  ),
+  (
+    'Chesse',
+    33.00,
+    'ua',
+    '2022-08-10',
+    1,
+    'two week'
   );
+  
